d204200ca29e65328ec928906197e4d8c6ff0369
Kris <kris.hunt@me.com>
2015-08-24 09:08:59 -0700

    Clean up extra files

--- FizzBuzz/FizzBuzz/main.m
+++ FizzBuzz/FizzBuzz/main.m
@@ -6,11 +6,25 @@
 //  Copyright (c) 2015 Krispy. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
-  @autoreleasepool {
-      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  
+  for (int number = 1; number <= 100; number++) {
+    
+    if (number % 3 == 0 && number % 5 ==0) {
+		    printf("FizzBuzz\n");
+    } else if (number % 3 == 0) {
+		    printf("Fizz\n");
+    } else if (number % 5 == 0) {
+		    printf("Buzz\n");
+    } else {
+		    printf("%i\n", number);
+    }
+  }
+  return 0;
+  
+}
   }
 }
